1c2e7b35901069d9beb85ffe77dd7ad08ad5a380
Minhaz Irphan <minhazmohamed@outlook.com>
2019-02-10 00:42:15 +0530

    Added the header comments

---
+++ optimizo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Optimizo
+ * Plugin URI:  https://www.optimizo.lk
+ * Description: Automatic optimization for your website
+ * Version:     0.0.1
+ * Author:      Minhaz Irphan
+ * Author URI:  https://minhaz.winauthority.com
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
