5fd4148fcd3abea688e0bf24f7b24d74df87eb24
Minhaz Irphan <minhazmohamed@outlook.com>
2019-02-27 02:23:31 +0530

    27-02-19 2:20am
    Added seperate functions in "class.optimiz.php" and tried ways to replace "WP-cache" if it already exists in the wp-config file. Tried using regex as well

--- class.optimizo.php
+++ class.optimizo.php
@@ -6,7 +6,58 @@
  * Time: 10:54 AM
  */
 
-function register_activation_hook($file, $function) {
-    $file = plugin_basename($file);
-    add_action('activate_' . $file, $function);
+class OptimizoClass{
+	function addToWPConfig(){
+
+		/*
+		 * This is a function which will be used to add anything to WordPress WP-Config file which includes all the configurations for a WordPress installation.
+		 * Currently it only includes the function to add the "WP-CACHE" as true in the file.
+		 */
+
+
+		$wp_config_file = @file_get_contents( ABSPATH . "wp-config.php" );
+
+		$wp_config_contents = @file_get_contents( ABSPATH . "wp-config.php" );
+
+		$word = "define('WP_CACHE', true);";
+
+		if(preg_match(".*".$word."*.", $wp_config_contents)){
+			$wp_config_file = @file_get_contents( ABSPATH . "wp-config.php" );
+			$wp_config_file = str_replace( "define('WP_CACHE', true);", "/** Optimizo's configuration for cache **/ \ndefine('WP_CACHE', false);\n\n", $wp_config_file);
+			if (! @file_put_contents( ABSPATH . "wp-config.php", $wp_config_file ) ) {
+
+			}
+		} else {
+			$wp_config_file = str_replace( "/** MySQL hostname */", "/** Optimizo's configuration for cache **/ \ndefine('WP_CACHE', true);\n\n/** MySQL hostname */", $wp_config_file );
+
+			if (! @file_put_contents( ABSPATH . "wp-config.php", $wp_config_file ) ) {
+
+			}
+		}
+	}
+
+	function writeToHtaccess(){
+
+		/*
+		 * This function will be used to write the rewrite access rules and other rules for caching and G-Zip to the server's .htaccess file
+		 */
+
+	}
+
+
+	function deactivate(){
+
+		/*
+		 * This is the function that will be used and called upon the deactivation of the plugin.
+		 * Currently this function removes the "WP-CACHE" statement from the WP-Config file if it's available.
+		 */
+
+		$wp_config_file = @file_get_contents( ABSPATH . "wp-config.php" );
+
+		$wp_config_file = str_replace( "/** Optimizo's configuration for cache **/ \ndefine('WP_CACHE', true);", null, $wp_config_file );
+
+		if ( ! @file_put_contents( ABSPATH . "wp-config.php", $wp_config_file ) ) {
+			add_action( 'admin_notices', 'failed_to_add_wp_config' );
+		}
+	}
 }
--- optimizo.php
+++ optimizo.php
@@ -3,7 +3,7 @@
  * Plugin Name: Optimizo
  * Plugin URI:  https://www.optimizo.lk
  * Description: Automatic optimization for your website
- * Version:     0.0.5
+ * Version:     0.0.6
  * Author:      Minhaz Irphan
  * Author URI:  https://minhaz.winauthority.com
  * License:     GPL2
@@ -11,108 +11,86 @@
  */
 
 
-if (!defined('ABSPATH')){
-    die;
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
 }
 
 
 class Optimizo {
-    function activation(){
-    	$path = ABSPATH;
-    	//Checking if it's installed in a sub-directory
+	function activation() {
+		$path = ABSPATH;
+		//Checking if it's installed in a sub-directory
 
-	    if($this->is_sub_directory_install()){
-	    	$path = $this->get_ABSPATH();
-	    }
+		if ( $this->is_sub_directory_install() ) {
+			$path = $this->get_ABSPATH();
+		}
 
-	    if (is_plugin_active($path.'/wp-content/plugins/Optimizo/optimizo.php')){
+		if ( is_plugin_active( $path . '/wp-content/plugins/Optimizo/optimizo.php' ) ) {
 
-	    } else {
-		    add_action( 'admin_notices', 'display_error_message' );
-	    }
+		} else {
+			add_action( 'admin_notices', 'display_error_message' );
+		}
 
-	    //Adding the default wp_cache in the wp_config file. This line will be added with a comment which will be easy for a user to identify that it's from this plugin
+		require_once( 'class.optimizo.php' );
 
-	    $wp_config_file = @file_get_contents(ABSPATH."wp-config.php");
-//
-//	    if (!str_replace("define('WP_CACHE', true);", "/** Optimizo's configuration for cache **/ \ndefine('WP_CACHE', true);\n\n", $wp_config_file)){
-//
-//
-//
-//        }
+		$optimizoClass = new OptimizoClass();
 
-	    $wp_config_file = str_replace("/** MySQL hostname */", "/** Optimizo's configuration for cache **/ \ndefine('WP_CACHE', true);\n\n/** MySQL hostname */", $wp_config_file);
+		$optimizoClass->addToWPConfig();
 
-	    if(!@file_put_contents(ABSPATH."wp-config.php", $wp_config_file)){
-		    add_action('admin_notices', 'failed_to_add_wp_config');
-	    }
+	}
 
-    }
+	function deactivation() {
+		require_once( 'class.optimizo.php' );
 
-    function deactivation(){
-	    //Adding the default wp_cache in the wp_config file. This line will be added with a comment which will be easy for a user to identify that it's from this plugin
+		$optimizoClass = new OptimizoClass();
 
-	    $wp_config_file = @file_get_contents(ABSPATH."wp-config.php");
+		$optimizoClass->deactivate();
+	}
 
-	    $wp_config_file = str_replace("/** Optimizo's configuration for cache **/ \ndefine('WP_CACHE', true);", null, $wp_config_file);
+	function uninstall() {
 
-	    if(!@file_put_contents(ABSPATH."wp-config.php", $wp_config_file)){
-		    add_action('admin_notices', 'failed_to_add_wp_config');
-	    }
-    }
-
-    function uninstall(){
-
-    }
+	}
 
 
-	public function is_sub_directory_install(){
-		if(strlen(site_url()) > strlen(home_url())){
+	function is_sub_directory_install() {
+		if ( strlen( site_url() ) > strlen( home_url() ) ) {
 			return true;
 		}
+
 		return false;
 	}
 
-	public function get_ABSPATH(){
-		$path = ABSPATH;
-		$websiteUrl = site_url();
+	function get_ABSPATH() {
+		$path           = ABSPATH;
+		$websiteUrl     = site_url();
 		$websiteHomeUrl = home_url();
-		$diff = str_replace($websiteHomeUrl, "", $websiteUrl);
-		$diff = trim($diff,"/");
+		$diff           = str_replace( $websiteHomeUrl, "", $websiteUrl );
+		$diff           = trim( $diff, "/" );
 
-		$pos = strrpos($path, $diff);
+		$pos = strrpos( $path, $diff );
 
-		if($pos !== false){
-			$path = substr_replace($path, "", $pos, strlen($diff));
-			$path = trim($path,"/");
-			$path = "/".$path."/";
+		if ( $pos !== false ) {
+			$path = substr_replace( $path, "", $pos, strlen( $diff ) );
+			$path = trim( $path, "/" );
+			$path = "/" . $path . "/";
 		}
+
 		return $path;
 	}
 
 }
 
-function failed_to_add_wp_config(){
+function display_message_on_activation() {
 	?>
     <div class="notice notice-info">
-        <p><?php _e( 'Optimizo has created wasn\'t able to edit your wp-config file.'); ?></p>
+        <p><?php _e( 'Thank you for installing & activating Optimizo. Your website is in good hands!' ); ?></p>
     </div>
 	<?php
 }
 
-function display_message_on_activation(){
-	?>
-	<div class="notice notice-info">
-		<p><?php _e( 'Thank you for installing & activating Optimizo. Your website is in good hands!'); ?></p>
-	</div>
-	<?php
-}
-
-if (class_exists('Optimizo')){
+if ( class_exists( 'Optimizo' ) ) {
 	add_action( 'admin_notices', 'display_message_on_activation' );
-    $optimizo = new Optimizo();
-} else {
-
+	$optimizo = new Optimizo();
 }
 
 register_activation_hook( __FILE__, array( $optimizo, 'activation' ) );
