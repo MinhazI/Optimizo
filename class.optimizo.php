0d051b524bdf177499524df928e509ca87adfb3e
Minhaz Irphan <minhazmohamed@outlook.com>
2019-03-03 02:10:02 +0530

    03-03-2019
    Added the rewrite code to the .htaccess part IF the WordPress install directorty doesn't include one.

--- class.optimizo.php
+++ class.optimizo.php
@@ -40,7 +40,17 @@ class OptimizoClass {
 		 */
 
 		if ( ! file_exists( ABSPATH . ".htaccess" ) ) {
-			$htaccessData = "# BEGIN Optimizo's rules"."\n".
+			$htaccessData = "# BEGIN WordPress"."\n".
+								'<IfModule mod_rewrite.c>'."\n".
+									'RewriteEngine On'."\n".
+									'RewriteBase /'."\n".
+									'RewriteRule ^index\.php$ - [L]'."\n".
+									'RewriteCond %{REQUEST_FILENAME} !-f'."\n".
+			                        'RewriteCond %{REQUEST_FILENAME} !-d'."\n".
+									'RewriteRule . /index.php [L]'."\n".
+								'</IfModule>'."\n".
+							"# END WordPress"."\n\n".
+							"# BEGIN Optimizo's rules"."\n".
 			                    '<FilesMatch "\.(webm|ogg|mp4|ico|pdf|flv|jpg|jpeg|png|gif|webp|js|css|swf|x-html|css|xml|js|woff|woff2|ttf|svg|eot)(\.gz)?$">'."\n".
 			                        '<IfModule mod_expires.c>'."\n".
 			                            'AddType application/font-woff2 .woff2'."\n".
@@ -169,16 +179,12 @@ class OptimizoClass {
 			                '</IfModule>'."\n".
 			                "# END Optimizo's rules"."\n";
 
-//			file_put_contents(ABSPATH . ".htaccess", $htaccessData);
-
 			$htaccessContents = @file_get_contents( ABSPATH . ".htaccess" );
 			$htaccessReplacement = str_replace( "# END WordPress", "# END WordPress\n\n".$htaccessData, $htaccessContents);
 
 			if ( ! @file_put_contents( ABSPATH . ".htaccess", $htaccessReplacement ) ) {
 
 			}
-
-//			fclose(ABSPATH . ".htaccess");
 		}
 
 	}
